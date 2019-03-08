2ff2501cc6b5315aeb645d22543de223c323fe79
Ad Friedman <adien330@yahoo.com>
2019-03-08 22:12:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,52 @@
 
 def key_for_min_value(name_hash)
 
+  ger = name_hash.to_a.flatten
+        i,arrs = 0, []
+        while i < ger.length
+            if i.odd?
+            arrs << ger[i]
+            end
+        i+=1
+end
+
+arrs  #[10,50,17]
+# arr = []
+# until i == 0
+
+#             if arrs[i] > arrs[i-1]
+#             arrs[i-1..i].pop
+#             elsif arrs[i-1] > arrs[i]
+#             arrs[i-1..i].shift
+
+#         end
+#         i-=1
+
+#     end
+
+arrs = arrs.map{|i|-i}
+arrs = arrs.max
+
+
+
+
+
+arrs = arrs.to_i * -1
+# arrs = arrs.join
+jim = name_hash.select{|i, v| v == arrs.to_i}
+jim = jim.to_a.flatten
+i, arrd = 0, ""
+while i < jim.length
+if i.even?
+ arrd = jim[i]
+end
+i+=1
+end
+
+if jim.empty?
+  p nil
+else
+p arrd
+end
+
 end
