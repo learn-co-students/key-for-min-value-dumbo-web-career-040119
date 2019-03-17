2dae8473d396e6bed35b6dfcb7e1d22d39a6354c
Adam Kearney <aklives@hotmail.com>
2019-03-17 23:43:49 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,25 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+if name_hash.empty? == true
+ return nil
+else
+  value = 0
+  age2 = 1000
+  value = name_hash.collect do |name, age|
+            if age < age2
+               age2 = age
+            end
+          end
+  value = age2
+  puts "value = " << value
+
+  name_hash.collect do |name, age|
+    if name_hash[name] == value
+      return name
+    end
+  end
+
+end
 
 end
