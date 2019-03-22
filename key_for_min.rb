19fd5226939db0f680970927dc027593abae0c79
Ad Friedman <adien330@yahoo.com>
2019-03-22 18:52:59 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,17 @@
 
 def key_for_min_value(name_hash)
   d = []
-  collect do |i,v|
+  name_hash.collect do |i,v|
   d << v * -1
   end
   b = ""
   b = d.max
 
-  name_hash.index(b * -1)
-
-  end
+  if name_hash != {}
+    b = b * -1
+    name_hash.key(b)
+        else
+        nil
+      end
 
 end
