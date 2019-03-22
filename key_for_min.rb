31535dcce50aaa9a6c1d4c43f35ed2d4b856576b
Ad Friedman <adien330@yahoo.com>
2019-03-22 18:37:32 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,53 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  d = []
+  collect do |i,v|
+  d << v * -1
+  end
+  b = ""
+  b = d.max
 
-  ger = name_hash.to_a.flatten
-        i,arrs = 0, []
-        while i < ger.length
-            if i.odd?
-            arrs << ger[i]
-            end
-        i+=1
-end
+  name_hash.index(b * -1)
 
-arrs  #[10,50,17]
-# arr = []
-# until i == 0
-
-#             if arrs[i] > arrs[i-1]
-#             arrs[i-1..i].pop
-#             elsif arrs[i-1] > arrs[i]
-#             arrs[i-1..i].shift
-
-#         end
-#         i-=1
-
-#     end
-
-arrs = arrs.map{|i|-i}
-arrs = arrs.max
-
-
-
-
-
-arrs = arrs.to_i * -1
-# arrs = arrs.join
-jim = name_hash.select{|i, v| v == arrs.to_i}
-jim = jim.to_a.flatten
-i, arrd = 0, ""
-while i < jim.length
-if i.even?
- arrd = jim[i]
-end
-i+=1
-end
-
-if jim.empty?
-  p nil
-else
-p arrd
-end
+  end
 
 end
